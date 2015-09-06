494a43cf4c0fdb8f50a66192d3025a34c757b4d2
Thomas Gläßle <t_glaessle@gmx.de>
2015-09-06 03:49:31 +0200

    Use /sys/class/backlight/... interface

--- brightness.lua
+++ brightness.lua
@@ -36,6 +36,19 @@ local function readcommand(command)
     return text
 end
 
+local function readfile(command)
+    local file = io.open(command)
+    local text = file:read('*all')
+    file:close()
+    return text
+end
+
+local function writefile(filename, text)
+  local file = io.open(filename, 'w')
+  file:write(text)
+  file:close()
+end
+
 local function quote(str)
     return "'" .. string.gsub(str, "'", "'\\''") .. "'"
 end
@@ -60,8 +73,9 @@ end
 function vcontrol.new(args)
     local sw = setmetatable({}, vcontrol.wmt)
 
-    sw.cmd = "xbacklight"
-    sw.step = args.step or '20'
+    sw.path = args.path or "/sys/class/backlight/intel_backlight"
+    sw.max_brightness = args.max_brightness or math.floor(tonumber(readfile(sw.path .. "/max_brightness")))
+    sw.step = args.step or math.ceil(0.2 * sw.max_brightness)
 
     sw.widget = wibox.widget.textbox()
     sw.widget.set_align("right")
@@ -85,23 +99,28 @@ function vcontrol:exec(...)
 end
 
 function vcontrol:get()
-    local brightness = math.floor(tonumber(self:exec("-get")))
-    self.widget:set_text(string.format(" [%3d] ", brightness))
-    return brightness
+    local absolute_brightness = tonumber(readfile(self.path .. "/brightness"))
+    local relative_brightness = absolute_brightness / self.max_brightness
+    local percentage = math.floor(100*relative_brightness)
+    self.widget:set_text(string.format(" [%3d] ", percentage))
+    return percentage
 end
 
-function vcontrol:set(brightness)
-    self:exec('-set', tostring(brightness))
+function vcontrol:set(percentage)
+    percentage = math.max(percentage, 0)
+    percentage = math.min(percentage, 100)
+    brightness = percentage*self.max_brightness/100
+    writefile(self.path .. "/brightness", tostring(brightness))
     self:get()
 end
 
 function vcontrol:up()
-    self:exec("-inc", self.step)
+    self:set(self:get() + self.step)
     self:get()
 end
 
 function vcontrol:down()
-    self:exec("-dec", self.step)
+    self:set(self:get() - self.step)
     self:get()
 end
 
